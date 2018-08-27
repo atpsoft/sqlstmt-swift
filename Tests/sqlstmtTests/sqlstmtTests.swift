788e526faf823ff0db8605e8cf1c365b2bcf991b
Makani Mason <gitauthor@pqmland.com>
2018-08-27 14:14:38 -0600

    test: split apart tests and give descriptive names

--- Tests/sqlstmtTests/sqlstmtTests.swift
+++ Tests/sqlstmtTests/sqlstmtTests.swift
@@ -2,23 +2,29 @@ import XCTest
 @testable import sqlstmt
 
 final class sqlstmtTests: XCTestCase {
-  func testExample() throws {
-    var sqlt = SqlStmt()
+  func testNoCalls() throws {
+    let sqlt = SqlStmt()
     XCTAssertThrowsError(try sqlt.to_s())
+  }
 
+  func testSimplestValidSelect() throws {
+    let sqlt = SqlStmt()
     sqlt.select()
     sqlt.table("source")
     sqlt.get("blah")
     sqlt.no_where()
+    XCTAssertEqual("SELECT blah FROM source", try sqlt.to_s())
+  }
 
-    // need to get this working, but for the moment the chaining doesn't work right
-    // sqlt = SqlStmt()
-    // sqlt.select().table("source").get("blah").no_where()
-
+  func testMethodChaining() throws {
+    let sqlt = SqlStmt()
+    sqlt.select().table("source").get("blah").no_where()
     XCTAssertEqual("SELECT blah FROM source", try sqlt.to_s())
   }
 
   static var allTests = [
-    ("testExample", testExample),
+    ("testNoCalls", testNoCalls),
+    ("testSimplestValidSelect", testSimplestValidSelect),
+    ("testMethodChaining", testMethodChaining),
   ]
 }
