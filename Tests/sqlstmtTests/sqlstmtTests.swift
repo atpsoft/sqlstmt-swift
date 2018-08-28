0d5302e1b01a9f63983c3dc6701f3552247e2c7c
Makani Mason <gitauthor@pqmland.com>
2018-08-28 12:25:00 -0600

    simplify / consolidate tests

--- Tests/sqlstmtTests/sqlstmtTests.swift
+++ Tests/sqlstmtTests/sqlstmtTests.swift
@@ -2,29 +2,17 @@ import XCTest
 @testable import sqlstmt
 
 final class sqlstmtTests: XCTestCase {
-  func testNoCalls() throws {
+  func testSelect() throws {
     let sqlt = SqlStmt()
     XCTAssertThrowsError(try sqlt.to_s())
-  }
 
-  func testSimplestValidSelect() throws {
-    let sqlt = SqlStmt()
-    sqlt.select()
-    sqlt.table("source")
-    sqlt.get("blah")
+    sqlt.select().table("source").get("blah")
     sqlt.no_where()
-    XCTAssertEqual("SELECT blah FROM source", try sqlt.to_s())
-  }
-
-  func testMethodChaining() throws {
-    let sqlt = SqlStmt()
-    sqlt.select().table("source").get("blah").no_where()
+    XCTAssertEqual(["source"], sqlt.data.table_ids)
     XCTAssertEqual("SELECT blah FROM source", try sqlt.to_s())
   }
 
   static var allTests = [
-    ("testNoCalls", testNoCalls),
-    ("testSimplestValidSelect", testSimplestValidSelect),
-    ("testMethodChaining", testMethodChaining),
+    ("testSelect", testSelect),
   ]
 }
