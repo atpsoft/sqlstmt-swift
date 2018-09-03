bd752d2020f937f676d0c6fd34c32f6099efac88
Makani Mason <gitauthor@pqmland.com>
2018-09-03 13:02:22 -0600

    add notes about missing tests

--- Tests/sqlstmtTests/toSqlTests.swift
+++ Tests/sqlstmtTests/toSqlTests.swift
@@ -3,13 +3,20 @@ import XCTest
 
 final class sqlstmtTests: XCTestCase {
   func testToSql() {
+    // TODO; test nil or document why we can't
+
     XCTAssertEqual("'blah'", "blah".toSql())
     XCTAssertEqual("'b\\\\lah'", "b\\lah".toSql())
     XCTAssertEqual("'b\\'lah'", "b'lah".toSql())
     XCTAssertEqual("'b\\\"lah'", "b\"lah".toSql())
 
+    // TODO: test numbers
+
     XCTAssertEqual("1", true.toSql())
     XCTAssertEqual("0", false.toSql())
+
+    // TODO: test date, datetime
+    // TODO: test array
   }
 
   static var allTests = [
